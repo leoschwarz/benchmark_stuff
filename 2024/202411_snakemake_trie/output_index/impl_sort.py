22375a3ea2772d8879997d9b24ff68808029e440
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-11-01 11:10:57 +0100

    initial commit

---
+++ 2024/202411_snakemake_trie/output_index/impl_sort.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+import bisect
+from collections import defaultdict
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from snakemake.rules import Rule
+
+
+class OutputIndex:
+    """Look up structure for rules, that can be queried by the output products which they create."""
+
+    def __init__(self, rules: list[Rule]) -> None:
+        entries = defaultdict(list)
+        for rule in rules:
+            for product in rule.products():
+                prefix = str(product.constant_prefix())
+                suffix = str(product.constant_suffix())
+                entries[prefix].append((rule, suffix))
+        self._entries = sorted(entries.items())
+
+    def match(self, targetfile: str) -> set[Rule]:
+        """Returns all rules that match the given target file, considering only the prefix and suffix up to the
+        first wildcard.
+
+        To further verify the match, the returned rules should be checked with ``Rule.is_producer(targetfile)``.
+        """
+        stop_idx = bisect.bisect_right(self._entries, targetfile, key=lambda x: x[0])
+        hits = set()
+
+        first = True
+        for index in range(stop_idx - 1, -1, -1):
+            key, entries = self._entries[index]
+            if targetfile.startswith(key):
+                hits.update(rule for rule, suffix in entries if targetfile.endswith(suffix))
+                first = False
+            elif not first:
+                break
+
+        return hits
+
+    def match_producer(self, targetfile: str) -> set[Rule]:
+        return {rule for rule in self.match(targetfile) if rule.is_producer(targetfile)}
